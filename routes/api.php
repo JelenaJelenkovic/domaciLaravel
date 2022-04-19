3f069bd71df40214979186d0eec4c45071a9cf1c
JelenaJelenkovic <57746585+JelenaJelenkovic@users.noreply.github.com>
2022-04-19 21:11:18 +0200

    v2
    izmene

--- routes/api.php
+++ routes/api.php
@@ -24,13 +24,7 @@ Route::get('/products/{id}', [ProductController::class, 'show']);
 Route::get('/products/search/{name}', [ProductController::class, 'search']);
 
 
-// Protected routes
-Route::group(['middleware' => ['auth:sanctum']], function () {
-    Route::post('/products', [ProductController::class, 'store']);
-    Route::put('/products/{id}', [ProductController::class, 'update']);
-    Route::delete('/products/{id}', [ProductController::class, 'destroy']);
-    Route::post('/logout', [AuthController::class, 'logout']);
-});
+
 
 
 
