7b30a82dde1fa0b12ef4809701b17d6dd33791bb
JelenaJelenkovic <57746585+JelenaJelenkovic@users.noreply.github.com>
2022-04-19 21:11:45 +0200

    v3
    Dodate protected rute

--- routes/api.php
+++ routes/api.php
@@ -24,7 +24,13 @@ Route::get('/products/{id}', [ProductController::class, 'show']);
 Route::get('/products/search/{name}', [ProductController::class, 'search']);
 
 
-
+// Protected routes
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::post('/products', [ProductController::class, 'store']);
+    Route::put('/products/{id}', [ProductController::class, 'update']);
+    Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
 
 
 
